dcc52e900f6a82e078f7d9dbbf141c1e27943c2c
Shakeeb <arsshakeeb149@protonmail.com>
2023-05-03 10:49:17 +0530

    Added Changes to 7-Store-API

--- 7-Store-API/controllers/products.js
+++ 7-Store-API/controllers/products.js
@@ -2,12 +2,13 @@ const Product = require('../models/product');
 
 const getAllProductsStatic = async (req, res) => {
 
-    const products = await Product.find({}).select('name price');
+    const products = await Product.find({});
     res.status(200).json({ products, nbHits: products.length });
+    console.log(req.query.numFilters);
 };
 
 const getAllProducts = async (req, res) => {
-    const { featured, company, search, sort, fields } = req.query;
+    const { featured, company, search, sort, fields, numericFilters } = req.query;
 
     const queryObject = {};
 
@@ -22,6 +23,28 @@ const getAllProducts = async (req, res) => {
     if (search) {
         queryObject.name = {$regex: search, $options:'i'};
     }
+
+    if (numericFilters) {
+        const operatorMap = {
+            '>':'$gt',
+            '>=':'$gte',
+            '=':'$eq',
+            '<':'$lt',
+            '<=':'$lte',
+        }
+
+        const regEx = /\b(<|>|<=|>=|=)\b/g;
+        let filters = numericFilters.replace(regEx, (match) => `-${operatorMap[match]}-`);
+        const options = ['price', 'rating'];
+        filters.split(',').forEach(filter => {
+            const [ field, operator, value] = filter.split('-');
+            if (options.includes(field)) {
+                queryObject[field] = {[operator]: Number(value)};
+            }
+        }); 
+    }
+
+    // console.log(queryObject);
 
     let result = Product.find(queryObject);
 
